990f015eff0baf9966c05b40d3932d8f7c23b45f
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-08 12:11:43 -0500

    Deeploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`React Mastermind is running on port ${port}...`)
+});
